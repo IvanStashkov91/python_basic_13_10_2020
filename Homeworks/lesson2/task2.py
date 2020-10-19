8047e12f7d20a020668c957a16228416eec7cd77
NeoKeen <ivanstashkov91@gmail.com>
2020-10-19 20:01:23 +0700

    task2

--- Homeworks/lesson2/task2.py
+++ Homeworks/lesson2/task2.py
@@ -5,32 +5,11 @@
 Для заполнения списка элементов необходимо использовать функцию input().
 '''
 
-while True:
-    el_count = input('Введите количество элементов в списке:\n>>> ')
-    if el_count.isdigit():
-        el_count = int(el_count)
-        break
-    else:
-        print('Ошибка ввода! Вы ввели не число')
-
-list = []
-i = 0
-
-while i < el_count:
-    list.append(input(f"Введите {i} - е значение списка:\n >>> "))
-    i += 1
+list = input('Введите значения через пробел:\n>>> ').split()
 
 i = 0
-if len(list) % 2 == 0:
-    while i < len(list):
-        el = list[i]
-        list[i] = list[i+1]
-        list[i+1] = el
-        i += 2
-else:
-    while i < len(list) - 1:
-        el = list[i]
-        list[i] = list[i + 1]
-        list[i + 1] = el
-        i += 2
+for item in list[1::2]:
+    list[i], list[i + 1] = list[i + 1], list[i]
+    i += 2
+
 print(list)
