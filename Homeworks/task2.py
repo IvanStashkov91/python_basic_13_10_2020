8368a83317d75bd05e5394c1fcf8b05dd2dbe018
NeoKeen <ivanstashkov91@gmail.com>
2020-10-26 23:29:50 +0700

    task2

--- Homeworks/task2.py
+++ Homeworks/task2.py
@@ -6,10 +6,12 @@
 Результат: [12, 44, 4, 10, 78, 123].
 '''
 
-list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+from function_list import my_enumerate
+
+list = [300, 333, 333, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
 # вариант 1
-result_list = [el for itm, el in enumerate(list[1:], 1) if el > list[itm - 1]]
+result_list = [el for itm, el in my_enumerate(list[1:], 1) if el > list[itm - 1]]
 
 print(result_list)
 
